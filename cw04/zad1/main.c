41ad3e2d405ee9e91b06ba742b6d0d2c4e7cfdf4
wolski0420 <wolski0420@gmail.com>
2020-03-31 23:43:13 +0200

    Finished 4.1

---
+++ cw04/zad1/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int stop_identifier = 0;    // this variable decide to continue or pause printing
+
+void sigstp_handle(){   // here we only stop printing
+    if(stop_identifier == 1){
+        stop_identifier = 0;
+    }
+    else{
+        stop_identifier = 1;
+        printf("Oczekuje na CTRL+Z - kontynuacja albo CTRL+C - zakonczenie programu\n");
+    }
+}
+
+void sigint_handle(){   // it stops program
+    printf("Odebrano sygnal SIGINT\n");
+    exit(0);
+}
+
+int main(int argc, char **argv){
+    // stop signal
+    struct sigaction actionSIG;
+    actionSIG.sa_handler = sigstp_handle;
+    actionSIG.sa_flags = 0;
+    sigemptyset(&actionSIG.sa_mask);
+    sigaction(SIGTSTP,&actionSIG,NULL);
+
+    // end signal
+    signal(SIGINT, sigint_handle);
+
+    // printing
+    while(1){
+        if(stop_identifier == 0){
+            system("ls -al");
+            sleep(1);
+        }
+        else{
+            pause();
+        }
+    }
+
+    return 0;
+}
